19a77c29f06f4bb4641ef13f260103a64f608a92
Gustavo Oliveira Garcia Ribeiro <gustavo.ribeiro@expocaccer.com.br>
2022-07-11 21:03:09 -0300

    Feat: Adicionando dados na api no componente.

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,9 +1,37 @@
-import { View, Text, TextInput } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, FlatList, SafeAreaView, Image, TouchableOpacity, ActivityIndicator } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
+
+import { format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+import userApi from '../../api/userApi';
 
 import styles from "./styles";
 
 export function Home() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+
+  async function getUsers() {
+    await userApi.get('')
+      .then(function (response) {
+        setUsers(response.data.results);
+    
+      })
+  }
+  async function handleLoadingMore() {
+    await userApi.get('')
+      .then(function (response) {
+        setUsers([...users, ...response.data.results]);
+      })
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Pacientes</Text>
@@ -14,12 +42,42 @@ export function Home() {
           <TextInput
             style={styles.searchBar}
             placeholder="Searching"
+            placeholderTextColor="#fff"
           />
-          <MaterialIcons name="person-search" size={24} color="black" style={styles.personSearchIcon}/>
+          <MaterialIcons name="person-search" size={24} color="white" style={styles.personSearchIcon} />
         </View>
 
-        <MaterialIcons name="filter-alt" size={35} color="black" style={styles.filterIcon}/>
+        <MaterialIcons name="filter-alt" size={35} color="white" style={styles.filterIcon} />
       </View>
+       {users.length > 0 && (
+        <FlatList
+          style={styles.cardContainer}
+          data={users}
+          keyExtractor={(item) => item.email}
+          renderItem={({ item }) => (
+            <View style={styles.card}>
+              <Image style={styles.avatar} source={{ uri: `${item.picture.large}` }} />
+              <View style={styles.patientInfo}>
+                <Text style={styles.textCard}>{`${item.name.title} ${item.name.last}`}</Text>
+                <Text style={styles.textCard}>{item.name.first}</Text>
+                <View style={styles.footerCard}>
+                  <Text style={styles.textCard}>{item.gender}</Text>
+                  <Text style={styles.textCard}>{format(new Date(item.dob.date), "dd'/'MM'/'yyyy", { locale: ptBR })}</Text>
+                </View>
+              </View>
+            </View>
+          )}
+          onEndReached={handleLoadingMore}
+          onEndReachedThreshold={0.1}
+          ListFooterComponent={
+            <View
+              style={styles.loadingMore}>
+              <MaterialIcons name="refresh" size={30} color="black" />
+              <Text style={{ fontSize: 25 }}>Loading more...</Text>
+            </View>
+          } 
+        />
+      )}
     </View>
   )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -7,11 +7,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         marginTop: 50,
+        backgroundColor: '#003461',
     },
     title: {
         fontSize: 20,
         textAlign: 'center',
         marginBottom: 50,
+        color: 'white',
     },
     filter: {
         flexDirection: 'row',
@@ -30,6 +32,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderRadius: 4,
         borderColor: '#ddd',
+        color: 'white',
         marginLeft: 10
     },
     personSearchIcon: {
@@ -37,8 +40,56 @@ const styles = StyleSheet.create({
         opacity: 0.5
     },
     filterIcon: {
-       marginRight: 15,
-       opacity: 0.8
+        marginRight: 15,
+        opacity: 0.8
+    },
+    cardContainer: {
+        flex: 1
+    },
+    card: {
+        width: width * 0.95,
+        marginLeft: 10,
+        marginTop: 10,
+        flexDirection: "row",
+        padding: 12,
+        backgroundColor: '#fff',
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+
+        elevation: 8,
+    },
+    textCard: {
+        color: '#003461',
+    },
+    footerCard: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    patientInfo: {
+        flex: 1,
+        justifyContent: 'space-around',
+        marginLeft: 12,
+    },
+    avatar: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+        borderColor: '#003461',
+        borderWidth: 2,
+    },
+    loadingMore: { 
+        marginBottom: 20, 
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: 'center',
+        marginTop: 10 
     }
 });
 
